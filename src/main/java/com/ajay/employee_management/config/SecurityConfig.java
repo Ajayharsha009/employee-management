c26574a7e2b07d87bedc9b1229ed0da2916584fd
V.Ajayharsha009 <ajayharshavarthan630@gmail.com>
2026-08-26 15:46:49 +0530

    Fix Railway CORS configuration

--- src/main/java/com/ajay/employee_management/config/SecurityConfig.java
+++ src/main/java/com/ajay/employee_management/config/SecurityConfig.java
@@ -81,7 +81,8 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "http://localhost:5500",
+                "https://employee-management-production-1a07.up.railway.app"
         ));
 
         configuration.setAllowedMethods(List.of(
